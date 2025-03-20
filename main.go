fba90938359c9a3056a6937b193c5600d51be919
John Moutafis <johnmoutafis@Johns-MacBook-Pro.local>
2025-03-20 15:05:13 +0200

    Ch-07: Flesh out the Manager

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
 
+	"cube/manager"
 	"cube/task"
 	"cube/worker"
 	workerApi "cube/worker/api"
@@ -34,13 +35,53 @@ func runTasks(w *worker.Worker) {
 func main() {
 	host := os.Getenv("CUBE_HOST")
 	port, _ := strconv.Atoi(os.Getenv("CUBE_PORT"))
+
 	fmt.Println("Starting Cube worker")
+
 	w := worker.Worker{
 		Queue: *queue.New(),
 		Db:    make(map[uuid.UUID]*task.Task),
 	}
 	api := workerApi.Api{Address: host, Port: port, Worker: &w}
+
 	go runTasks(&w)
 	go w.CollectStats()
-	api.Start()
+	go api.Start()
+
+	workers := []string{fmt.Sprintf("%s:%d", host, port)}
+	m := manager.NewManager(workers)
+
+	// Adding tasks to the Manager
+	for i := 0; i < 3; i++ {
+		t := task.Task{
+			ID:    uuid.New(),
+			Name:  fmt.Sprintf("test-container-%d", i),
+			State: task.Scheduled,
+			Image: "strm/helloworld-http",
+		}
+		te := task.TaskEvent{
+			ID:    uuid.New(),
+			State: task.Running,
+			Task:  t,
+		}
+		m.AddTask(te)
+		m.SendWork()
+	}
+
+	// Anonymous function
+	go func() {
+		for {
+			fmt.Printf("[Manager] Updating tasks from %d workers\n", len(m.Workers))
+			m.UpdateTasks()
+			time.Sleep(15 * time.Second)
+		}
+	}()
+
+	for {
+		for _, t := range m.TaskDb {
+			fmt.Printf("[Manager] Task: id: %s, state: %d\n", t.ID, t.State)
+			time.Sleep(15 * time.Second)
+		}
+	}
+
 }
